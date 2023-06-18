a6779c4c2de7e33c5534a49c0f46a5e177d5cbd3
ackermanjayjay <reza23198@gmail.com>
2023-06-18 14:03:24 +0700

    props data atau melempar data menuju component card

---
+++ src/components/contents/Content.jsx
@@ -0,0 +1,7 @@
+export const Content = [{
+    id: 0, // Used in JSX as a key
+    name: 'Muhammad Reza',
+    profession: 'College Student Informatic Engineering',
+    Skill: 'NLP, Network Administrator',
+    imageId: 'OnoXjy3'
+  }];
