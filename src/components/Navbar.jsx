7b31a8e5757a1e7da3e138f14c385c846954c152
ackermanjayjay <reza23198@gmail.com>
2023-06-18 14:04:42 +0700

    memperbaharui tampilan navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 function Navbar(){
     return(
         <>
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-300">
   <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <a className="btn btn-ghost normal-case text-xl base-300">daisyUI</a>
   </div>
   <div className="flex-none gap-2">
     <div className="form-control">
-      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
     </div>
     <div className="dropdown dropdown-end">
       <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
@@ -17,7 +16,7 @@ function Navbar(){
       </label>
       <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
         <li>
-          <a className="justify-between">
+          <a className="justify-between ">
             Profile
             <span className="badge">New</span>
           </a>
